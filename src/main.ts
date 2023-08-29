62fd6b9d24001130e501dc7cb4914243ec3f9aa2
walidmahade <walidmahade@gmail.com>
2023-08-29 21:57:49 +0600

    added scroll to top button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-import "./styles/style.css";
+// scroll to top
+const scrollToTopTrigger = document.querySelector("#scroll-to-top");
+
+if (scrollToTopTrigger) {
+  scrollToTopTrigger.addEventListener("click", () => {
+    console.log("click");
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+}
